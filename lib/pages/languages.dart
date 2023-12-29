01a93f2345997ee1296afa689f215f8782914e5d
frank gimeno <frankgimeno3@gmail.com>
2023-12-29 10:58:36 +0100

    añado dos idiomas a languages

--- lib/pages/languages.dart
+++ lib/pages/languages.dart
@@ -14,6 +14,23 @@ class LanguagesPage extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               Text('Languages Page', style: TextStyle(fontSize: 24, color: Colors.white)),
+              SizedBox(height: 20), // Adds spacing between the text and the buttons
+              ElevatedButton(
+                onPressed: () {
+                  // Add your logic for the English button
+                  print('English selected');
+                },
+                child: Text('English'),
+              ),
+              SizedBox(height: 10), // Adds spacing between the buttons
+              ElevatedButton(
+                onPressed: () {
+                  // Add your logic for the Español button
+                  print('Español selected');
+                },
+                child: Text('Español'),
+              ),
+              SizedBox(height: 20), // Adds spacing between the buttons and Go Back button
               ElevatedButton(
                 onPressed: () => Navigator.pop(context),
                 child: Text('Go Back'),
